b571ca8766016867815fc6adea053143cf84c1b9
Dimuthchathu101 <dimuthchathu101@gmail.com>
2024-01-09 19:24:34 +0530

    Model New

--- module_3_4/preparing_data_CNN.py
+++ module_3_4/preparing_data_CNN.py
@@ -5,6 +5,8 @@ from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
 
 pixel_width = 28
 pixel_height = 28
+batch_size = 32
+epochs = 10
 
 num_of_classes = 10
 
@@ -28,8 +30,31 @@ labels_test = keras.utils.to_categorical(labels_test, num_of_classes)
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-print("POST CONV2D", model.output_shape)
 model.add(MaxPooling2D(pool_size=(2, 2)))
-print("POST MaxPooling2D", model.output_shape)
 model.add(Dropout(0.25))
-print("POST Dropout", model.output_shape)
+model.add(Flatten())
+model.add(Dense(128,activation='relu'))
+model.add(Dense(num_of_classes, activation='softmax'))
+
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(features_train, labels_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(features_test, labels_test)
+          )
+
+score = model.evaluate(features_test, labels_test, verbose=0)
+
+model.save('C:\\Users\\USER\\Documents\\GitHub\\ML_Python_Test\\module_3_4\\handwriting_model.h5')
+
+import coremltools
+coreml_model = coremltools.converters.keras.convert(model,input_names = ['image'],image_input_names = 'image')
+
+coreml_model.author = 'Dimuth C Bandara'
+coreml_model.license = 'MIT'
+coreml_model.short_description = 'Hello World'
