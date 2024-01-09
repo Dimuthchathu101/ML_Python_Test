6118ee416255f88a98caee2111bd234204a5af10
Dimuthchathu101 <dimuthchathu101@gmail.com>
2024-01-09 18:22:30 +0530

    Preparing Data CNN

---
+++ module_3_4/preparing_data_CNN.py
@@ -0,0 +1,27 @@
+import keras
+from keras.datasets import mnist
+
+pixel_width = 28
+pixel_height = 28
+
+num_of_classes = 10
+
+(features_train, labels_train), (features_test, labels_test) = mnist.load_data()
+
+features_train = features_train.reshape(features_train.shape[0], pixel_width, pixel_height, 1)
+features_test = features_test.reshape(features_test.shape[0], pixel_width, pixel_height, 1)
+
+input_shape = (pixel_width, pixel_height, 1)
+
+features_train = features_train.astype('float32')
+features_test = features_test.astype('float32')
+
+print(features_train[0])
+
+features_train /= 255
+features_test /= 255
+
+labels_train = keras.utils.to_categorical(labels_train, num_of_classes)
+labels_test = keras.utils.to_categorical(labels_test, num_of_classes)
+
+print(labels_train[5])
