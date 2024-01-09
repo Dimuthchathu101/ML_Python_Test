a01e7f0724a7948bce5a74cca093ecd45602d6c1
Dimuthchathu101 <dimuthchathu101@gmail.com>
2024-01-09 12:46:13 +0530

    Iris Flower Example Test Dataset

--- day_01/iris_flowers_test.py
+++ day_01/iris_flowers_test.py
@@ -1,6 +1,7 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 iris_dataset = datasets.load_iris()
 
@@ -16,4 +17,15 @@ my_classifier.fit(features_train, labels_train)
 
 prediction = my_classifier.predict(features_test)
 
-print(prediction)
+print(accuracy_score(labels_test, prediction))
+
+#Versicolor
+iris1 = [[4.7, 2.5, 3.1, 1.2]]
+iris_prediction = my_classifier.predict(iris1)
+
+if iris_prediction[0] == 0:
+    print("Setosa")
+if iris_prediction[0] == 1:
+    print("Versicolor")
+if iris_prediction[0] == 2:
+    print("Virginica")
